d29658ebec72bb6296a601fc4873dfaa56ca3c09
Oliver Wright <meiamsome@meiamso.me>
2022-02-20 21:31:29 +0000

    Add tests for more value types

---
+++ test/integration/valueTypes.test.ts
@@ -0,0 +1,88 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { execute, GraphQLSchema, parse } from 'graphql';
+import { createSchema } from '../../src';
+
+describe('JraphQL Value Type', () => {
+    const schema = createSchema((file) => readFileSync(join(__dirname, 'documents', file)).toString()) as GraphQLSchema;
+
+    it('can get sub value type fields.', () => {
+        const result = execute({
+            schema,
+            document: parse(`
+                query {
+                    valueTypes {
+                        booleanValue
+                        intValue
+                        nullableValue
+                        stringValue
+                    }
+                }
+            `),
+        });
+
+        expect(result).toEqual({
+            data: {
+                valueTypes: [
+                    {
+                        booleanValue: false,
+                        intValue: 1,
+                        nullableValue: 1,
+                        stringValue: 'test1',
+                    },
+                    {
+                        booleanValue: true,
+                        intValue: 2,
+                        nullableValue: null,
+                        stringValue: 'test2',
+                    },
+                ],
+            },
+        });
+    });
+
+    it('can descend multiple layers', () => {
+        const result = execute({
+            schema,
+            document: parse(`
+                query {
+                    valueTypes {
+                        valueType {
+                            valueType {
+                                booleanValue
+                                intValue
+                                nullableValue
+                                stringValue
+                                valueType {
+                                    booleanValue
+                                }
+                            }
+                        }
+                    }
+                }
+            `),
+        });
+
+        expect(result).toEqual({
+            data: {
+                valueTypes: [
+                    {
+                        valueType: null,
+                    },
+                    {
+                        valueType: {
+                            valueType: {
+                                booleanValue: true,
+                                intValue: 4,
+                                nullableValue: null,
+                                stringValue: 'test4',
+                                valueType: null,
+                            },
+                        },
+                    },
+                ],
+            },
+        });
+    });
+});
